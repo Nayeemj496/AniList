c5d10f72ce475b2ddb9c8be1964d8123f3f16d73
Nayeem <nayeemj496@gmail.com>
2024-02-29 21:36:26 +0600

    review's creation date fixed

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -85,6 +85,25 @@ const animeIndividualReviewControllerGET = async (req, res) => {
         )
     ).rows[0];
 
+    let time = new Date() - review.DATE_OF_CREATION_ANIME;
+    let timeString = "";
+
+    let seconds = Math.floor(time / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+    let months = Math.floor(days / 30);
+    let years = Math.floor(months / 12);
+
+    if (years) timeString += `${years} years ago`;
+    else if (months) timeString += `${months} months ago`;
+    else if (days) timeString += `${days} days ago`;
+    else if (hours) timeString += `${hours} hours ago`;
+    else if (minutes) timeString += `${minutes} minutes ago`;
+    else timeString += `${seconds} seconds ago`;
+
+    review.time = timeString;
+
     if (req.session.user) {
         let isLiked =
             (
@@ -200,6 +219,25 @@ const mangaIndividualReviewControllerGET = async (req, res) => {
             { outFormat: oracledb.OUT_FORMAT_OBJECT }
         )
     ).rows[0];
+
+    let time = new Date() - review.DATE_OF_CREATION_MANGA;
+    let timeString = "";
+
+    let seconds = Math.floor(time / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+    let months = Math.floor(days / 30);
+    let years = Math.floor(months / 12);
+
+    if (years) timeString += `${years} years ago`;
+    else if (months) timeString += `${months} months ago`;
+    else if (days) timeString += `${days} days ago`;
+    else if (hours) timeString += `${hours} hours ago`;
+    else if (minutes) timeString += `${minutes} minutes ago`;
+    else timeString += `${seconds} seconds ago`;
+
+    review.time = timeString
 
     if (req.session.user) {
         let isLiked =
